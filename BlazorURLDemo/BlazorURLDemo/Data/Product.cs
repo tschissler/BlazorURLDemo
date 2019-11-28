4cb325ac8268c9b2c103145609cbc421375e33e5
ThomasSchissler <tschissler@agilemax.de>
2019-11-28 13:01:32 +0100

    Initial version of Demo

---
+++ BlazorURLDemo/BlazorURLDemo/Data/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorURLDemo.Data
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+    }
+}
